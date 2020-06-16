f78021db1d6b02ce7f72b190e93ef8c3c978eb0d
scrappy-strategy-4846 <tomlai8882000@hotmail.com>
2020-06-16 18:24:19 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,30 @@
+require 'pry'
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  
+  row_index = 0 
+  string_only = []
+
+  while row_index < src.length do
+     
+    element_index = 0 
+    while element_index < src[row_index].length do
+      
+      if src[row_index][element_index].class == String
+        
+        string_only << src[row_index][element_index]
+        
+      end
+      
+      element_index += 1 
+    end 
+    
+    row_index += 1 
+    
+    
+  end 
+
+ string_only.join(" ")
 end
+
